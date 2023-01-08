e1d6d0e1e5ccbb984623410bc18a29f8d85365b7
Muneeb Khan <96231592+muneeb-i-khan@users.noreply.github.com>
2023-01-08 19:37:44 +0530

    LFGGGGG

---
+++ pages/api/generate.js
@@ -0,0 +1,31 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix =
+`
+Write me a rap in the style of any one of  Kenrick Lamar, J Cole or Emminem with the title below. 
+
+Title:
+`
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.8,
+    max_tokens: 350,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
